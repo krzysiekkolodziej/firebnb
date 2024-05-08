b9f292c735932945350fcf434de542daff2f731f
krzysiekkolodziej <krzysiek.kolodziej23@gmail.com>
2024-05-08 21:11:42 +0200

    UnauthenticatedApp React

--- client/react/src/libs/feature-shell/unauthenticated-app/pages/login.tsx
+++ client/react/src/libs/feature-shell/unauthenticated-app/pages/login.tsx
@@ -1,10 +1,12 @@
-import { hotelRoom } from "@firebnb/public/assets";
+import { hotelRoom, logoIcon } from "@firebnb/public/assets";
 
 export const Login = () => {
   return (
-    <div className="flex">
-      <div></div>
-      <div>
+    <div className="bg-blue-400 w-full flex">
+      <div className="w-1/2 flex flex-col space-y-2">
+        <img src={logoIcon} />
+      </div>
+      <div className="w-1/2">
         <img src={hotelRoom} />
       </div>
     </div>
