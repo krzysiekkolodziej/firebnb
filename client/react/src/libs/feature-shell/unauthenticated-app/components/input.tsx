d23eb6b63ba12683b825440b088449fe62f1e66b
krzysiekkolodziej <krzysiek.kolodziej23@gmail.com>
2024-05-08 21:33:17 +0200

    Show and hide password

--- client/react/src/libs/feature-shell/unauthenticated-app/components/input.tsx
+++ client/react/src/libs/feature-shell/unauthenticated-app/components/input.tsx
@@ -1,6 +1,7 @@
-import { ComponentPropsWithRef, forwardRef } from "react";
+import { ComponentPropsWithRef, forwardRef, useState } from "react";
 import { FieldError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+import { IconEye, IconEyeSlash } from "../../../utils/icons";
 
 export type InputProps = ComponentPropsWithRef<"input"> & {
   error?: FieldError;
@@ -9,6 +10,12 @@ export type InputProps = ComponentPropsWithRef<"input"> & {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ name, error, className, disabled, ...props }, ref) => {
+    const [_type, _setType] = useState(props.type);
+
+    const handleButtonClick = () => {
+      _setType((prev) => (prev === "text" ? "password" : "text"));
+    };
+
     const inputClass = twMerge(
       "rounded-md border border-gray-200 text-gray-600 text-sm leading-md px-3 py-2 placeholder:text-gray-400",
       "focus:outline-0 focus:ring-0 focus:border-gray-400 hover:border-gray-400",
@@ -18,15 +25,27 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     );
 
     return (
-      <input
-        className={inputClass}
-        aria-invalid={!!error}
-        aria-describedby={error ? `${name}-error` : undefined}
-        ref={ref}
-        name={name}
-        disabled={disabled}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          {...props}
+          type={_type}
+          className={inputClass}
+          aria-invalid={!!error}
+          aria-describedby={error ? `${name}-error` : undefined}
+          ref={ref}
+          name={name}
+          disabled={disabled}
+        />
+        {props.type === "password" && (
+          <button
+            className="absolute right-4 top-4"
+            type="button"
+            onClick={handleButtonClick}
+          >
+            {_type === "password" ? <IconEyeSlash /> : <IconEye />}
+          </button>
+        )}
+      </div>
     );
   }
 );
