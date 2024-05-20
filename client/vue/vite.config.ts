b449ec9f6cc59b0fee32302a48385c89ad96b6f5
krzysiekkolodziej <krzysiek.kolodziej23@gmail.com>
2024-05-20 19:56:11 +0200

    Vue project skeleton

--- client/vue/vite.config.ts
+++ client/vue/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  plugins: [
+    vue(),
+    viteTsConfigPaths({
+      root: "../../",
+    }),
+  ],
+});
